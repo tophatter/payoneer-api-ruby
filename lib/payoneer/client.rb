13886c62cc745551ff24091b95581bdd1bf82390
Sophia L Wu <sophia.luxi.wu@gmail.com>
2017-12-14 12:51:56 -0800

    Add extended payout method

--- lib/payoneer/client.rb
+++ lib/payoneer/client.rb
@@ -39,6 +39,54 @@ module Payoneer
       )
     end
 
+    # Includes additional items as needed to be Payoneer SAFE compliant (https://app.asana.com/0/440902128870182/473284740903863/f)
+    def expanded_payout(payee_id:, client_reference_id:, amount:, currency: 'USD', description:, payout_date: Time.now, seller_id:, seller_name:, seller_url:, seller_type: 'ECOMMERCE', path:)
+      params = {
+        payee_id: payee_id,
+        client_reference_id: client_reference_id,
+        amount: amount,
+        currency: currency,
+        description: description,
+        payout_date: payout_date.strftime('%Y-%m-%d'),
+        orders_report: {
+          merchant: {
+            id: seller_id,
+            store: {
+              name: seller_name,
+              url: seller_url,
+              type: seller_type
+            }
+          },
+          orders: {
+            type: 'url',
+            path: path,
+            credentials: { # @TODO: What type of credentials? When should we expire?
+                type: 'AUTHORIZATION',
+                token: 'FILL'
+            }
+          }
+        }
+      }
+
+      encoded_credentials = 'Basic ' + Base64::encode64("#{configuration.username}:#{configuration.api_password}").chomp
+      response = RestClient.post "#{configuration.json_base_uri}/payouts", params.to_json, { content_type: 'application/json', accept: :json, Authorization: encoded_credentials }
+      raise ResponseError.new(code: response.code, body: response.body) if response.code != 200
+
+      hash = JSON.parse(response.body)
+
+      if hash.key?('Code')
+        Response.new(hash['Code'], hash['Description'])
+      else
+        hash = if block_given?
+          yield(hash)
+        else
+          hash
+        end
+
+        Response.new(Response::OK_STATUS_CODE, hash)
+      end
+    end
+
     def payout_details(payee_id:, payment_id:)
       post('GetPaymentStatus', p4: payee_id, p5: payment_id)
     end
