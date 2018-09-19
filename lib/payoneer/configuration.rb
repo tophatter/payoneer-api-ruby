b4cda8c4c3af9ea1cd87dac4bc1a8f0076ac6f81
Todd Eichel <todd@toddeichel.com>
2018-09-19 12:05:00 -0700

    Allow passing a custom API host to config object (#4)
    * Allow passing a custom API host to config object
    
    Also clean up config options handling.
    
    * Version bump for custom host config option

--- lib/payoneer/configuration.rb
+++ lib/payoneer/configuration.rb
@@ -2,29 +2,35 @@ module Payoneer
   class Configuration
     attr_reader :partner_id, :username, :api_password, :auto_approve_sandbox_accounts, :http_client_options
 
-    def initialize(partner_id:, username:, api_password:, environment: 'development', auto_approve_sandbox_accounts: true, http_client_options: {})
+    def initialize(partner_id:, username:, api_password:, environment: 'development', protocol: 'https', host: nil, http_client_options: {}, auto_approve_sandbox_accounts: true)
       @partner_id                    = partner_id
       @username                      = username
       @api_password                  = api_password
-      @host                          = 'api.sandbox.payoneer.com' if environment != 'production'
-      @auto_approve_sandbox_accounts = auto_approve_sandbox_accounts && environment != 'production'
+      @environment                   = environment
+
+      @protocol                      = protocol
+      @host                          = host || default_host
       @http_client_options           = http_client_options
-    end
 
-    def protocol
-      @protocol || 'https'
-    end
-
-    def host
-      @host || 'api.payoneer.com'
+      @auto_approve_sandbox_accounts = auto_approve_sandbox_accounts && environment != 'production'
     end
 
     def xml_base_uri
-      @xml_base_uri || "#{protocol}://#{host}/Payouts/HttpApi/API.aspx"
+      "#{@protocol}://#{@host}/Payouts/HttpApi/API.aspx"
     end
 
     def json_base_uri
-      @json_base_uri || "#{protocol}://#{host}/v2/programs/#{@partner_id}"
+      "#{@protocol}://#{@host}/v2/programs/#{@partner_id}"
+    end
+
+    private
+
+    def default_host
+      if @environment == 'production'
+        'api.payoneer.com'
+      else
+        'api.sandbox.payoneer.com'
+      end
     end
   end
 end
